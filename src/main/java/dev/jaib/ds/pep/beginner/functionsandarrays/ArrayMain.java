1de824eec6e44eebfd2dad09b3579e9ae200276b
jaibhardwaj <jai.bhardwaj99@gmail.com>
2022-05-10 23:43:23 +0530

    Implemented Subsets of an Array

--- src/main/java/dev/jaib/ds/pep/beginner/functionsandarrays/ArrayMain.java
+++ src/main/java/dev/jaib/ds/pep/beginner/functionsandarrays/ArrayMain.java
@@ -5,8 +5,14 @@ public class ArrayMain
 
 	public static void main(String[] args)
 	{
+		System.out.println("########### barChart ###########");
 		barChart(new int[]{3, 1 , 0, 7, 5});
+
+		System.out.println("########### printSubArraysOfAnArray ###########");
 		printSubArraysOfAnArray(new int[]{1, 2, 3});
+
+		System.out.println("########### printSubsetsOfArray ###########");
+		printSubsetsOfArray(new int[]{1, 2, 3});
 	}
 	/**
 	 * Span Of Array
@@ -618,6 +624,88 @@ public class ArrayMain
 				}
 				System.out.println();
 			}
+		}
+	}
+
+	/**
+	 * Subsets Of Array
+	 * Easy
+	 *
+	 * 1. You are given a number n, representing the count of elements.
+	 * 2. You are given n numbers.
+	 * 3. You are required to print all subsets of arr. Each subset should be
+	 * on separate line. For more clarity check out sample input and output.
+	 *
+	 * Constraints
+	 * 1 <= n <= 10
+	 * 0 <= n1, n2, .. n elements <= 10^3
+	 *
+	 * Format
+	 * Input
+	 * A number n
+	 * n1
+	 * n2
+	 * .. n number of elements
+	 *
+	 * Output
+	 * [Tab separated elements of subset]
+	 * ..
+	 * All subsets
+	 *
+	 * Example
+	 * Sample Input
+	 *
+	 * 3
+	 * 10
+	 * 20
+	 * 30
+	 *
+	 * Sample Output
+	 * -	-	-
+	 * -	-	30
+	 * -	20	-
+	 * -	20	30
+	 * 10	-	-
+	 * 10	-	30
+	 * 10	20	-
+	 * 10	20	30
+	 *
+	 * @param arr the array
+	 */
+	public static void printSubsetsOfArray(int[] arr)
+	{
+		int limit = (int) Math.pow (2, arr.length); //calculating the number of subsets
+
+		for (int i = 0; i < limit; i++)
+		{
+			//convert i to binary and use 0's and 1's
+			//to check if an array's element is to be printed or not
+			StringBuilder set = new StringBuilder();
+			// we make use of set to print in required order
+			int temp = i;
+			// we store i because we need to use value
+			// of i without manipulating the actual i
+			// as i is the outer loop iterator
+			for (int j = arr.length - 1; j >= 0; --j)
+			{
+				//calculating the binary, extracting //
+				// the remainder one and by one
+				// and putting required element
+				// in the String to be printed.
+				int rem = temp % 2;
+				temp = temp / 2;
+				if (rem == 0)   //nothing to be printed
+				{
+					set.insert(0, "-" + "\t");
+				}
+				else
+				{
+					// we print the element, so we add it to our answer string
+					set.insert(0, arr[j] + "\t");
+				}
+			}
+			System.out.println(set);
+			// printing the required pattern line-by-line
 		}
 	}
 
