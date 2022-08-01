38ebff489d64d4b5fc1fb78f0e8d4fcabee66988
jaibhardwaj <jai.bhardwaj99@gmail.com>
2022-08-02 00:33:31 +0530

    Implemented Display Array In Reverse Using Recursion

--- src/main/java/dev/jaib/ds/pep/beginner/recursion/RecursionMain.java
+++ src/main/java/dev/jaib/ds/pep/beginner/recursion/RecursionMain.java
@@ -4,7 +4,18 @@ public class RecursionMain
 {
 	public static void main(String[] args)
 	{
+		System.out.println("###### Tower of Hanoi ######");
 		towerOfHanoi(3, 10, 11, 12);
+
+		int[] arr = {10, 11, 12, 13, 14, 15};
+		System.out.println("###### Display Array ######");
+		displayArray(arr, 0);
+
+		System.out.println("###### Display Array in Reverse ######");
+		displayArrayInReverse(arr, 5);
+
+		System.out.println("###### Display Array in Reverse 2 ######");
+		displayArrayInReverse2(arr, 0);
 	}
 
 	/**
@@ -118,4 +129,79 @@ public class RecursionMain
 		displayArray(arr, idx + 1);
 
 	}
+
+	/**
+	 *
+	 * Display Array In Reverse
+	 * Easy
+	 *
+	 * 1. You are given a number n, representing the size of array a.
+	 * 2. You are given n numbers, representing elements of array a.
+	 * 3. You are required to print the elements of array from end to beginning each in a separate line.
+	 * 4. For the above purpose complete the body of displayArrReverse function. Don't change the signature.
+	 *
+	 * Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+	 *
+	 * Constraints
+	 * 1 <= n <= 30
+	 * 0 <= n1, n2, .. n elements <= 10
+	 *
+	 * Format
+	 * Input
+	 * A number n
+	 * n1
+	 * n2
+	 * .. n number of elements
+	 *
+	 * Output
+	 * n1
+	 * n2
+	 * .. n elements
+	 *
+	 * Example
+	 * Sample Input
+	 *
+	 * 5
+	 * 3
+	 * 1
+	 * 0
+	 * 7
+	 * 5
+	 *
+	 * Sample Output
+	 * 5
+	 * 7
+	 * 0
+	 * 1
+	 * 3
+	 *
+	 * @param arr the give array
+	 * @param idx the index
+	 */
+	public static void displayArrayInReverse(int[] arr, int idx)
+	{
+		if( idx < 0 )
+		{
+			return;
+		}
+
+		System.out.println(arr[idx]);
+		displayArrayInReverse(arr, idx - 1);
+
+	}
+
+	public static void displayArrayInReverse2(int[] arr, int idx)
+	{
+		if( idx == arr.length )
+		{
+			return;
+		}
+
+		displayArrayInReverse2(arr, idx + 1);
+		System.out.println(arr[idx]);
+
+
+	}
+
+
 }
