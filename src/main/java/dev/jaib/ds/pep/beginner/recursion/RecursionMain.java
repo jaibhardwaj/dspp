2d3c6fe340d3686481408120b156c449027485f0
jaibhardwaj <jai.bhardwaj99@gmail.com>
2022-08-02 19:12:58 +0530

    Implemented Max of an Array Using Recursion and its unit test

--- src/main/java/dev/jaib/ds/pep/beginner/recursion/RecursionMain.java
+++ src/main/java/dev/jaib/ds/pep/beginner/recursion/RecursionMain.java
@@ -199,8 +199,70 @@ public class RecursionMain
 
 		displayArrayInReverse2(arr, idx + 1);
 		System.out.println(arr[idx]);
+	}
 
 
+	/**
+	 *
+	 * Max Of An Array
+	 * Easy
+	 *
+	 * 1. You are given a number n, representing the count of elements.
+	 * 2. You are given n numbers.
+	 * 3. You are required to find the maximum of input.
+	 * 4. For the purpose complete the body of maxOfArray function. Don't change the signature.
+	 *
+	 * Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+	 *
+	 * Constraints
+	 * 1 <= n <= 10^4
+	 * 0 <= n1, n2, .. n elements <= 10 ^9
+	 *
+	 * Format
+	 * Input
+	 * A number n
+	 * n1
+	 * n2
+	 * .. n number of elements
+	 *
+	 * Output
+	 * A number representing max
+	 *
+	 * Example
+	 * Sample Input
+	 *
+	 * 6
+	 * 15
+	 * 30
+	 * 40
+	 * 4
+	 * 11
+	 * 9
+	 *
+	 * Sample Output
+	 * 40
+	 *
+	 * @param arr the given array
+	 * @param idx the index
+	 * @return the max element from array
+	 */
+	public static int maxOfArray(int[] arr, int idx)
+	{
+
+		if( idx == arr.length - 1 )
+		{
+			return arr[idx];
+		}
+		int misa = maxOfArray(arr, idx + 1);
+
+		if( misa > arr[idx] )
+		{
+			return misa;
+		}
+		else
+		{
+			return arr[idx];
+		}
 	}
 
 
--- src/test/java/dev/jaib/ds/pep/beginner/recursion/RecursionMainTest.java
+++ src/test/java/dev/jaib/ds/pep/beginner/recursion/RecursionMainTest.java
@@ -1,5 +1,15 @@
 package dev.jaib.ds.pep.beginner.recursion;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RecursionMainTest
 {
+	@Test
+	public void maxOfArray()
+	{
+		int[] arr = {15, 30, 40, 4, 11, 9};
+		assertEquals(40, RecursionMain.maxOfArray(arr, 0));
+	}
 }
