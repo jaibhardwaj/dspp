6b07baffe783372a4366165425ab6021eb322e2f
jaibhardwaj <jai.bhardwaj99@gmail.com>
2022-08-02 22:08:04 +0530

    mplemented First Index of an Element from an Array Using Recursion and its unit test

--- src/test/java/dev/jaib/ds/pep/beginner/recursion/RecursionMainTest.java
+++ src/test/java/dev/jaib/ds/pep/beginner/recursion/RecursionMainTest.java
@@ -12,4 +12,13 @@ public class RecursionMainTest
 		int[] arr = {15, 30, 40, 4, 11, 9};
 		assertEquals(40, RecursionMain.maxOfArray(arr, 0));
 	}
+
+	@Test
+	public void firstIndex()
+	{
+		int[] arr = {15, 30, 40, 4, 11, 9, 4, 11};
+		assertEquals(3, RecursionMain.firstIndex(arr, 0, 4));
+		assertEquals(4, RecursionMain.firstIndex(arr, 0, 11));
+		assertEquals(-1, RecursionMain.firstIndex(arr, 0, 1));
+	}
 }
