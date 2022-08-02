f2c25de4d493b83c077c041da6f47aedd045145a
jaibhardwaj <jai.bhardwaj99@gmail.com>
2022-08-03 00:24:49 +0530

    Implemented All Indices of an Element from an Array Using Recursion and its unit test

--- src/main/java/dev/jaib/ds/pep/beginner/recursion/RecursionMain.java
+++ src/main/java/dev/jaib/ds/pep/beginner/recursion/RecursionMain.java
@@ -404,5 +404,84 @@ public class RecursionMain
 		}
 	}
 
+	/**
+	 *
+	 * All Indices Of Array
+	 * Easy
+	 *
+	 * 1. You are given a number n, representing the count of elements.
+	 *
+	 * 2. You are given n numbers.
+	 *
+	 * 3. You are given a number x.
+	 *
+	 * 4. You are required to return the all indices at which x occurs in array a.
+	 *
+	 * 5. Return an array of appropriate size which contains all indices at which x occurs in array
+	 *
+	 * 6. If no such element exist print "NO OUTPUT" a.
+	 *
+	 *
+	 *
+	 * Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+	 *
+	 *
+	 * Constraints
+	 * 1 <= n <= 10^4 0 <= n1, n2, .. n elements <= 10 ^ 3 0 <= x <= 10 ^ 3
+	 *
+	 *
+	 * Format
+	 * Input
+	 * A number n n1 n2 .. n number of elements A number x
+	 *
+	 *
+	 * Output
+	 * Return the array of indices from the allIndices function. Display is managed for you.
+	 *
+	 *
+	 * Example
+	 * Sample Input
+	 *
+	 * 6
+	 * 15
+	 * 11
+	 * 40
+	 * 4
+	 * 4
+	 * 9
+	 * 4
+	 *
+	 * Sample Output
+	 * 3
+	 * 4
+	 *
+	 * @param arr the array
+	 * @param x the element
+	 * @param idx the index
+	 * @param fsf the indices found so far
+	 * @return the array containing the indices of elements in array
+	 */
+	public static int[] allIndices(int[] arr, int x, int idx, int fsf)
+	{
+		if( idx == arr.length )
+		{
+			return new int[fsf];
+		}
+
+		int[] iarr;
+
+		if( arr[idx] == x )
+		{
+			iarr = allIndices(arr, x, idx + 1, fsf + 1);
+			iarr[fsf] = idx;
+		}
+		else
+		{
+			iarr = allIndices(arr, x, idx + 1, fsf);
+		}
+
+		return iarr;
+	}
+
 
 }
--- src/test/java/dev/jaib/ds/pep/beginner/recursion/RecursionMainTest.java
+++ src/test/java/dev/jaib/ds/pep/beginner/recursion/RecursionMainTest.java
@@ -2,6 +2,7 @@ package dev.jaib.ds.pep.beginner.recursion;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RecursionMainTest
@@ -29,5 +30,14 @@ public class RecursionMainTest
 		assertEquals(6, RecursionMain.lastIndex(arr, 0, 4));
 		assertEquals(7, RecursionMain.lastIndex(arr, 0, 11));
 		assertEquals(-1, RecursionMain.lastIndex(arr, 0, 1));
+	}
+
+	@Test
+	public void allIndices()
+	{
+		int[] arr = {15, 30, 40, 4, 11, 9, 4, 11};
+		assertArrayEquals(new int[]{3, 6}, RecursionMain.allIndices(arr, 4, 0, 0));
+		assertArrayEquals(new int[]{4, 7}, RecursionMain.allIndices(arr, 11, 0, 0));
+		assertArrayEquals(new int[]{}, RecursionMain.allIndices(arr, 1, 0, 0));
 	}
 }
