cf6242dafb6ef8411a0cc23659203a4f7e36bf8f
jaibhardwaj <jai.bhardwaj99@gmail.com>
2022-08-04 23:25:38 +0530

    Implemented Print SpringPaths Using Recursion

--- src/main/java/dev/jaib/ds/pep/beginner/recursion/RecursionMain.java
+++ src/main/java/dev/jaib/ds/pep/beginner/recursion/RecursionMain.java
@@ -31,6 +31,9 @@ public class RecursionMain
 		System.out.println("###### Print KPC ######");
 		printKPC("78", "");
 
+		System.out.println("###### Print Stair Paths ######");
+		printStairPaths(4, "");
+
 	}
 
 	/**
@@ -977,6 +980,57 @@ public class RecursionMain
 		{
 			printKPC(ros, asf + val);
 		}
+	}
+
+	/**
+	 * Print Stair Paths
+	 * <p>
+	 * Easy
+	 * <p>
+	 * 1. You are given a number n representing number of stairs in a staircase.
+	 * 2. You are standing at the bottom of staircase. You are allowed to climb 1 step, 2 steps or 3 steps in one move.
+	 * 3. Complete the body of printStairPaths function - without changing signature - to print the list of all paths that can be used to climb the staircase up.
+	 * Use sample input and output to take idea about output.
+	 * <p>
+	 *
+	 * Constraints
+	 * 0 <= n <= 10
+	 * <p>
+	 * Format
+	 * Input
+	 * A number n
+	 * <p>
+	 * Output
+	 * Print paths (one path in each line) in order hinted by Sample output
+	 * <p>
+	 * Example
+	 * Sample Input
+	 * <p>
+	 * 3
+	 * <p>
+	 * Sample Output
+	 * 111
+	 * 12
+	 * 21
+	 * 3
+	 * @param n the stairs in staircase
+	 * @param path the path to climb
+	 */
+	public static void printStairPaths(int n, String path)
+	{
+		if( n == 0 )
+		{
+			System.out.println(path);
+			return;
+		}
+		else if( n < 0 )
+		{
+			return;
+		}
+
+		printStairPaths(n - 1, path + "1");
+		printStairPaths(n - 2, path + "2");
+		printStairPaths(n - 3, path + "3");
 	}
 
 }
