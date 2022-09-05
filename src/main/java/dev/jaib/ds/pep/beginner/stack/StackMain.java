8ae11c10efa4b30d4ee7bceaf378bd08c0704b4f
jaibhardwaj <jai.bhardwaj99@gmail.com>
2022-09-06 00:03:54 +0530

    DSPP-141, DSPP-144: Implemented Balanced Brackets and its unit test.

--- src/main/java/dev/jaib/ds/pep/beginner/stack/StackMain.java
+++ src/main/java/dev/jaib/ds/pep/beginner/stack/StackMain.java
@@ -18,23 +18,23 @@ public class StackMain
 	 * <p>
 	 * 4. You are required to print true if you detect extra brackets and false otherwise. e.g.' ((a + b) + (c + d)) -> false (a + b) + ((c + d)) -> true
 	 * <p>
-	 *
+	 * <p>
 	 * Constraints
 	 * <p>
 	 * 0 <= str.length <= 100
 	 * <p>
-	 *
+	 * <p>
 	 * Format
 	 * Input
 	 * <p>
 	 * A string str
 	 * <p>
-	 *
+	 * <p>
 	 * Output
 	 * <p>
 	 * true or false
 	 * <p>
-	 *
+	 * <p>
 	 * Example
 	 * Sample Input
 	 * <p>
@@ -77,4 +77,101 @@ public class StackMain
 
 		return false;
 	}
+
+	/**
+	 * Balanced Brackets
+	 * <p>
+	 * Easy
+	 * <p>
+	 * 1. You are given a string exp representing an expression.
+	 * <p>
+	 * 2. You are required to check if the expression is balanced i.e. closing brackets and opening brackets match up well.
+	 * <p>
+	 * e.g.
+	 * [(a + b) + {(c + d) * (e / f)}] -> true
+	 * <p>
+	 * [(a + b) + {(c + d) * (e / f)]} -> false
+	 * <p>
+	 * [(a + b) + {(c + d) * (e / f)} -> false
+	 * <p>
+	 * ([(a + b) + {(c + d) * (e / f)}] -> false
+	 * <p>
+	 * Constraints
+	 * <p>
+	 * 0 <= str.length <= 100
+	 * <p>
+	 * Format
+	 * Input
+	 * <p>
+	 * A string str
+	 * <p>
+	 * Output
+	 * <p>
+	 * true or false
+	 * <p>
+	 * Example
+	 * Sample Input
+	 * <p>
+	 * [(a + b) + {(c + d) * (e / f)}]
+	 * <p>
+	 * Sample Output
+	 * <p>
+	 * true
+	 *
+	 * @param str the string
+	 * @return true or false
+	 */
+	public static boolean balancedBrackets(String str)
+	{
+		Stack<Character> st = new Stack<>();
+
+		for( int i = 0; i < str.length(); i++ )
+		{
+			char ch = str.charAt(i);
+			if( ch == '(' || ch == '[' || ch == '{' )
+			{
+				st.push(ch);
+			}
+			else if( ch == ')' )
+			{
+				boolean val = handleClosing(st, '(');
+				if( !val )
+				{
+					return false;
+				}
+			}
+			else if( ch == ']' )
+			{
+				boolean val = handleClosing(st, '[');
+				if( !val )
+				{
+					return false;
+				}
+			}
+			else if( ch == '}' )
+			{
+				boolean val = handleClosing(st, '{');
+				if( !val )
+				{
+					return false;
+				}
+			}
+		}
+
+		return st.empty();
+	}
+
+	public static boolean handleClosing(Stack<Character> st, char corresopch)
+	{
+		if( st.size() == 0 || st.peek() != corresopch )
+		{
+			return false;
+		}
+		else
+		{
+			st.pop();
+		}
+		return true;
+	}
+
 }
--- src/test/java/dev/jaib/ds/pep/beginner/stack/StackMainTest.java
+++ src/test/java/dev/jaib/ds/pep/beginner/stack/StackMainTest.java
@@ -18,4 +18,24 @@ public class StackMainTest
 		boolean str2Result = StackMain.duplicateBrackets(str2);
 		assertTrue(str2Result);
 	}
+
+	@Test
+	public void balancedBracketsTest()
+	{
+		String str1 = "[(a+b)+{(c+d)*(e/f)}]";
+		boolean str1Result = StackMain.balancedBrackets(str1);
+		assertTrue(str1Result);
+
+		String str2 = "[(a+b)+{(c+d)*(e/f)]}";
+		boolean str2Result = StackMain.balancedBrackets(str2);
+		assertFalse(str2Result);
+
+		String str3 = "[(a+b)+{(c+d)*(e/f)}";
+		boolean str3Result = StackMain.balancedBrackets(str3);
+		assertFalse(str3Result);
+
+		String str4 = "([(a+b)+{(c+d)*(e/f)}]";
+		boolean str4Result = StackMain.balancedBrackets(str4);
+		assertFalse(str4Result);
+	}
 }
