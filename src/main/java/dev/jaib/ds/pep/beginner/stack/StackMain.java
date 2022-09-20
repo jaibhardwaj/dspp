b5facba8655e2de298a8eae2809d2d60cd81519f
jaibhardwaj <jai.bhardwaj99@gmail.com>
2022-09-20 23:19:55 +0530

    Implemented Merge Overlapping Interval.

--- src/main/java/dev/jaib/ds/pep/beginner/stack/StackMain.java
+++ src/main/java/dev/jaib/ds/pep/beginner/stack/StackMain.java
@@ -1,5 +1,6 @@
 package dev.jaib.ds.pep.beginner.stack;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class StackMain
@@ -11,6 +12,7 @@ public class StackMain
 
 		print(stockSpan(new int[]{9, 5, 2, 3, 7, 12, 6, 8, 1}));
 
+		mergeOverlappingIntervals(new int[][]{{1, 8}, {5, 12}, {14, 19}, {22, 28}, {25, 27}, {27, 30}});
 	}
 
 
@@ -1113,5 +1115,189 @@ public class StackMain
 		}
 
 		return pot;
+	}
+
+	public static void mergeOverlappingIntervalsWithSortedTime(int[][] arr)
+	{
+		// merge overlapping intervals and print in increasing order of start time
+		// First of all this Array Matrix must be sorted for this
+
+		Stack<Integer> start = new Stack<>();
+		Stack<Integer> end = new Stack<>();
+
+		start.push(arr[arr.length - 1][0]);
+		end.push(arr[arr.length - 1][1]);
+
+		for( int i = arr.length - 2; i >= 0; i-- )
+		{
+			if( arr[i][1] >= start.peek() )
+			{
+				start.pop();
+				start.push(arr[i][0]);
+			}
+			else
+			{
+				start.push(arr[i][0]);
+				end.push(arr[i][1]);
+			}
+		}
+
+		while(!start.empty() && !end.empty())
+		{
+			System.out.println(start.pop() + " " + end.pop());
+		}
+
+	}
+
+	/**
+	 * Merge Overlapping Interval
+	 * <p>
+	 * Medium
+	 * <p>
+	 * 1. You are given a number n, representing the number of time-intervals.
+	 * <p>
+	 * 2. In the next n lines, you are given a pair of space separated numbers.
+	 * <p>
+	 * 3. The pair of numbers represent the start time and end time of a meeting (first number is start time and second number is end time)
+	 * <p>
+	 * 4. You are required to merge the meetings and print the merged meetings output in increasing order of start time.
+	 * <p>
+	 *
+	 * E.g. Let us say there are 6 meetings
+	 * <p>
+	 * 1 8
+	 * <p>
+	 * 5 12
+	 * <p>
+	 * 14 19
+	 * <p>
+	 * 22 28
+	 * <p>
+	 * 25 27
+	 * <p>
+	 * 27 30
+	 * <p>
+	 * Then the output of merged meetings will belongs
+	 * <p>
+	 * 1 12
+	 * <p>
+	 * 14 19
+	 * <p>
+	 * 22 30
+	 * <p>
+	 * Note -> The given input may not be sorted by start-time.
+	 * <p>
+	 * Constraints
+	 * <p>
+	 * 1 <= n <= 10^4
+	 * <p>
+	 * 0 <= ith start time < 100
+	 * <p>
+	 * ith start time < ith end time <= 100
+	 * <p>
+	 * Format
+	 * Input
+	 * <p>
+	 * Input is managed for you
+	 * <p>
+	 * Output
+	 * <p>
+	 * Print a merged meeting start time and end time separated by a space in a line
+	 *  print all merged meetings one in each line.
+	 * <p>
+	 * Example
+	 * <p>
+	 * Sample Input
+	 * <p>
+	 * 6
+	 * <p>
+	 * 22 28
+	 * <p>
+	 * 1 8
+	 * <p>
+	 * 25 27
+	 * <p>
+	 * 14 19
+	 * <p>
+	 * 27 30
+	 * <p>
+	 * 5 12
+	 * <p>
+	 * Sample Output
+	 * <p>
+	 * 1 12
+	 * <p>
+	 * 14 19
+	 * <p>
+	 * 22 30
+	 *
+	 * @param arr the given array of start time and end time
+	 */
+	public static void mergeOverlappingIntervals(int[][] arr)
+	{
+		Pair[] pairs = new Pair[arr.length];
+		for( int i = 0; i < arr.length; i++ )
+		{
+			pairs[i] = new Pair(arr[i][0], arr[i][1]);
+		}
+
+		Arrays.sort(pairs);
+		Stack<Pair> st = new Stack<>();
+		for( int i = 0; i < pairs.length; i++ )
+		{
+			if( i == 0 )
+			{
+				st.push(pairs[i]);
+			}
+			else
+			{
+				Pair top = st.peek();
+				if( pairs[i].st > top.et )
+				{
+					st.push(pairs[i]);
+				}
+				else
+				{
+					top.et = Math.max(top.et, pairs[i].et);
+				}
+			}
+		}
+
+		Stack<Pair> rs = new Stack<>();
+		while(st.size() > 0)
+		{
+			rs.push(st.pop());
+		}
+
+		while(rs.size() > 0)
+		{
+			Pair p = rs.pop();
+			System.out.println(p.st + " " + p.et);
+		}
+
+	}
+
+	public static class Pair implements Comparable<Pair>
+	{
+		int st;
+		int et;
+
+		Pair(int st, int et)
+		{
+			this.st = st;
+			this.et = et;
+		}
+
+		public int compareTo(Pair other)
+		{
+			if( this.st != other.st )
+			{
+				return this.st - other.st;
+			}
+			else
+			{
+				return this.et - other.et;
+			}
+		}
 	}
 }
